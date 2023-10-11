74c22997fa5105ffa33ebbc1716913a0bdc0bf24
49173 <jessekroll2@gmail.com>
2023-10-11 12:38:10 +0200

    adding mysql db cnnocetion

--- museum.py
+++ museum.py
@@ -1,4 +1,29 @@
 import numpy as np
+import pymysql
+
+
+conn = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='password',
+    db='mydatabase',
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor
+)
+#how to insert result into db
+try:
+    with conn.cursor() as cursor:
+        # Create a new record
+        sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
+        cursor.execute(sql, ('john@example.com', 'mypassword'))
+
+    # Commit changes
+    conn.commit()
+
+    #print("Record inserted successfully")
+finally:
+    conn.close()
+
 pieces_number = 15
 class artwork:
     categories = {}
@@ -13,17 +38,36 @@ class artwork:
             
     def iscorrectguess(category,name,artist):
         try:
-            correct_guess = artork.categories[category][name]
+            correct_guess = artwork.categories[category][name]
         except:
             print("you are trying to access a non existing entry in the guess")
         if correct_guess == "AI" and artist == "AI":
+            guess_v= True
             return True
         elif correct_guess != "AI" and artist != "AI":
+            guess_v= True
             return True
         else:
+            guess_v= False
             return False
         
-        
+        try:
+           with conn.cursor() as cursor:
+        # Create a new record
+               sql = "INSERT INTO `users` (`guess`) VALUES (%s, %s)"
+               if guess_v== True: 
+                   x= "True"
+               else: 
+                   x= "False"
+
+               cursor.execute(sql, (x))
+
+    # Commit changes
+           conn.commit()
+
+    #print("Record inserted successfully")
+        finally:
+            conn.close()
 class player:
     def __init__(name):
         self.name = name
